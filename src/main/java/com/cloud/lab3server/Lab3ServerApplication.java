00edde7aecab68e5950abe24070d24ee93cb018b
Rafael Ramirez Lozano <rafaelrl85@hotmail.com>
2017-10-15 20:34:19 -0500

    adding comments on how to run it

--- src/main/java/com/cloud/lab3server/Lab3ServerApplication.java
+++ src/main/java/com/cloud/lab3server/Lab3ServerApplication.java
@@ -8,6 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class Lab3ServerApplication {
 
+	//Run this on localhost 8001 and go to http://localhost:8001/lab-3-client/default
+	//lab 2 client is the name of the app defined on the remote server config file, default is the well...default profile
 	public static void main(String[] args) {
 		SpringApplication.run(Lab3ServerApplication.class, args);
 	}
